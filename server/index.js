e6115a6d2afbcd6878e9192de17c2ac2d3edeb1f
Max Han <mwellh84@gmail.com>
2020-02-25 08:54:57 -0500

    update start-dev scripts and fix typos in server index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const path = require('path')
 
-app.use(morgan(dev))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '../public')));
 
 // routers
-app.use('/api', require('./api'))
+// app.use('/api', require('./api'))
 
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
