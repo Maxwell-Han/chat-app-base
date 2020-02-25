43c84b42e1de25b251e4f26a7a8aaa1d44afef3c
Max Han <mwellh84@gmail.com>
2020-02-25 08:51:03 -0500

    set up express

---
+++ server/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+router.use('/users', require('./users'))
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+module.exports = router
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(morgan(dev))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, '../public')));
+
+// routers
+app.use('/api', require('./api'))
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Starting up server from server/index.js");
+  console.log(`Your server, listening on port ${port}`);
+});
