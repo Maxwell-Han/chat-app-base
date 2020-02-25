cf48f98b9a87a3e162835d0c60088d4feddcb95b
Max Han <mwellh84@gmail.com>
2020-02-24 22:45:20 -0500

    setup basic react with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  entry: "./public/index.js",
-  mode: "development",
+  entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
+  mode: 'development',
   output: {
-    path: __dirname, // assumes bundle.js will also be in the root of project folder
-    filename: "bundle.js"
+    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
+    filename: './public/bundle.js'
   },
-  devtool: "source-maps",
+  devtool: 'source-maps',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loader: 'babel-loader'
       }
     ]
   }
