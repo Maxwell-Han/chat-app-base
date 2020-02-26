4218a48048a882fc1bac4c68f675c6f0d115bf4f
Max Han <mwellh84@gmail.com>
2020-02-25 20:28:36 -0500

    add connect mongo for persistent sessions

--- server/index.js
+++ server/index.js
@@ -1,57 +1,64 @@
-const express = require('express')
-const passport = require('passport')
-const session = require('express-session')
-const app = express()
-const morgan = require('morgan')
-const path = require('path')
-const { db, User } = require('./db')
-const secrets = require('../secrets')
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const app = express();
+const morgan = require("morgan");
+const path = require("path");
+const { db, User } = require("./db");
+const secrets = require("../secrets");
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.use(session({
-  secret: secrets.sessionsSecret,
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 7 * 24 * 60 * 60
+    }),
+    secret: process.env.SESSION_SECRET || secrets.sessionsSecret,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.serializeUser((user, done) => {
-  console.log('SERIALIZEING USER ', user, 'done is ', done)
-  done(null, user._id)
-})
+  console.log("SERIALIZEING USER ", user, "done is ", done);
+  done(null, user._id);
+});
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await User.findById(id)
-    done(null, user)
+    const user = await User.findById(id);
+    done(null, user);
   } catch (err) {
-    done(err)
+    done(err);
   }
-})
+});
 
 // routers
-app.use('/api', require('./api'))
-app.use('/auth', require('./auth'))
+app.use("/api", require("./api"));
+app.use("/auth", require("./auth"));
 
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-})
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
 
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.error(err);
   console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
+  res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log("Starting up server from server/index.js");
   console.log(`Your server, listening on port ${port}`);
 });
-
