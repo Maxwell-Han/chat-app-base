ca99b3bde04c673561f014913f76e279fd21e183
Max Han <mwellh84@gmail.com>
2020-04-16 23:05:18 -0400

    UI Revisions: update top nav

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -2,35 +2,39 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { logout } from "../store";
 import { Link } from "react-router-dom";
-import { Navbar } from "shards-react";
+import { Menu, Layout } from "antd";
+const { Header } = Layout;
+const topNavStyles = {display: 'flex', justifyContent: 'space-between'}
 
 class TopNav extends Component {
   render() {
     const { handleLogout } = this.props;
     return (
-      <Navbar>
-        <Link to="/main">Main Menu</Link>
-        <Link to="/login">Login</Link>
-        <a href="#" onClick={handleLogout}>
-          Logout
-        </a>
-      </Navbar>
+      <Header>
+         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]} style={topNavStyles}>
+          <Link to="/main">Main Menu</Link>
+          <Link to="/login">Login</Link>
+          <a href="#" onClick={handleLogout}>
+            Logout
+          </a>
+        </Menu>
+      </Header>
     );
   }
 }
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     isLoggedIn: !!state.user.id,
-    user: state.user
+    user: state.user,
   };
 };
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleLogout() {
       dispatch(logout());
-    }
+    },
   };
 };
 
